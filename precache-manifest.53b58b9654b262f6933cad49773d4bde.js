a882f5f05651a9c7cc9fbd10f00d5fedabea3cc9
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-05-04 19:29:00 +0100

    Updates

---
+++ precache-manifest.53b58b9654b262f6933cad49773d4bde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5373ee676efc42c0228",
+    "url": "/BookStore-App/static/css/main.47ba6cde.chunk.css"
+  },
+  {
+    "revision": "b5373ee676efc42c0228",
+    "url": "/BookStore-App/static/js/main.fe75a080.chunk.js"
+  },
+  {
+    "revision": "ec20ef9416097b8cc32f",
+    "url": "/BookStore-App/static/js/runtime~main.e4039ffe.js"
+  },
+  {
+    "revision": "892f179fb0afd489e791",
+    "url": "/BookStore-App/static/js/2.743a4d5c.chunk.js"
+  },
+  {
+    "revision": "40d0e08a7fdbf784502c0231a54fa8a5",
+    "url": "/BookStore-App/index.html"
+  }
+];
