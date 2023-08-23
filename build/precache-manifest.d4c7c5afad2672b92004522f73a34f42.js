f390c1dd1d3d88b5266adab934e3dcf8b6145020
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-08-23 11:59:36 +0100

    Remove build from gitignore file

---
+++ build/precache-manifest.d4c7c5afad2672b92004522f73a34f42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12cf296f03b82cd50dc3",
+    "url": "/BookStore-App/static/css/main.47ba6cde.chunk.css"
+  },
+  {
+    "revision": "12cf296f03b82cd50dc3",
+    "url": "/BookStore-App/static/js/main.01433b4b.chunk.js"
+  },
+  {
+    "revision": "ec20ef9416097b8cc32f",
+    "url": "/BookStore-App/static/js/runtime~main.e4039ffe.js"
+  },
+  {
+    "revision": "588884959e18aec1beba",
+    "url": "/BookStore-App/static/js/2.1c2f8ddc.chunk.js"
+  },
+  {
+    "revision": "bd41fd1f371047bb0fc8636c548e91e7",
+    "url": "/BookStore-App/index.html"
+  }
+];
